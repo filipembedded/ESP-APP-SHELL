47b1d9922101287b76d63b4a8196474ffdf91228
Dzo <mileticveljko99@gmail.com>
2024-06-25 16:59:45 +0200

    Found main.c and linker.ld for esp32

--- Core/App/TestApp/main.c
+++ Core/App/TestApp/main.c
@@ -1,11 +1,37 @@
-#include "ll/gpio_ll.h"
+#include <stdlib.h>
+#include <string.h>
 
-#define BLINK_GPIO 2
+int main();
 
-int main(void)
-{
-    while (1) {
-        
-    }
+extern int _bss_start;
+extern int _bss_end;
+extern int _rtc_bss_start;
+extern int _rtc_bss_end;
+extern int _init_start;
+
+/* CPU0 entry point (PRO CPU) */
+void __attribute__((noreturn)) call_start_cpu0(void){
+  //cpu_hal_set_vecbase(&_init_start);
+  memset(&_bss_start, 0, (&_bss_end - &_bss_start) * sizeof(_bss_start));
+ // esp_config_instruction_cache_mode();
+  (void) main();
 }
 
+/* CPU1 entry point (APP CPU) */
+void __attribute__((noreturn)) call_start_cpu1(void){
+
+  while(1) 
+  {
+
+  }
+}
+
+/* IRAM_ATTR for interrupt functions */
+
+int __attribute__((noreturn)) main() {
+
+  while(1) {
+
+
+  }
+}
